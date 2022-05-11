4168ac59817b6b808966dc7ca0c63a493abc8ca1
Nihad Abou-Zid <nihad@prosaldo.net>
2022-05-11 16:58:43 +0200

    create RecommenderSystem project

---
+++ Engine/src/test/java/org/camunda/bpm/wfm/shampoome/ShampooMeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.camunda.bpm.wfm.shampoome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShampooMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
