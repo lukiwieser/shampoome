8b694e877b537ec8b3200402d3fd1bd3f493d1a9
Tobias Henöckl <t.henoeckl@gmail.com>
2022-05-18 13:23:49 +0200

    added a shampoo object and some business logic, so the ingredients and the description changes depending on what the user answered to our nine questions

---
+++ recommender-system/src/main/java/Shampoo.java
@@ -0,0 +1,51 @@
+public class Shampoo {
+
+    private String name;
+    private String ingredients;
+    private String description;
+    private String size;
+    private Integer cost;
+
+    public Shampoo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+}
