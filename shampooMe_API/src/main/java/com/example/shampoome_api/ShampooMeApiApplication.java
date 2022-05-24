cab1bf29b3fb292a96138c76d509d6852e01c363
Nihad Abou-Zid <nihad@prosaldo.net>
2022-05-24 23:52:22 +0200

    created new api project

---
+++ shampooMe_API/src/main/java/com/example/shampoome_api/ShampooMeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shampoome_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShampooMeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShampooMeApiApplication.class, args);
+    }
+
+}
---
+++ shampooMe_API/src/test/java/com/example/shampoome_api/ShampooMeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shampoome_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShampooMeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
