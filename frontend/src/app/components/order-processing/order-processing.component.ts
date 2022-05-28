128d6b7e772d4804268569610033cc4ac726727c
Lukas Wieser <lukas.luki.wieser@gmail.com>
2022-05-28 19:27:30 +0200

    Added content to the order-processing page

--- frontend/src/app/components/order-processing/order-processing.component.ts
+++ frontend/src/app/components/order-processing/order-processing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-order-processing',
@@ -7,13 +8,26 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./order-processing.component.scss']
 })
 export class OrderProcessingComponent implements OnInit {
+  processId! : string | null;
+  orderId!: string;
+  estimatedDeliveryTime: Number = 26; 
 
   constructor(
-    private titleService: Title
+    private titleService: Title,
+    private route: ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
-    this.titleService.setTitle("Processing Order | ShampooMe")
+    this.titleService.setTitle("Processing Order | ShampooMe");
+    this.route.paramMap.subscribe(params => {
+      this.processId = params.get("processId");
+
+      //this.orderId = "asdf557";
+    });
   }
 
+  get orderLink() : string {
+    const domainAndPort = location.origin;
+    return domainAndPort + "/order-status/" + this.orderId;
+  }
 }
