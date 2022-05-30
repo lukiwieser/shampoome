15fa1f8b1e6815ff63571870fbb7695444a87313
Nihad Abou-Zid <nihad@prosaldo.net>
2022-05-30 23:31:31 +0200

    added http origin to corsConfiguration

--- shampooMe_API/src/main/java/com/example/shampoome_api/controller/RestController.java
+++ shampooMe_API/src/main/java/com/example/shampoome_api/controller/RestController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -19,6 +16,7 @@ import java.sql.*;
 import java.util.logging.Logger;
 
 @org.springframework.web.bind.annotation.RestController
+@CrossOrigin(origins = "http://wfm.ngdevs.net")
 public class RestController {
 
     private final String camundaEndpoint = "http://lva924-server3.ec.tuwien.ac.at:8081/engine-rest/";
