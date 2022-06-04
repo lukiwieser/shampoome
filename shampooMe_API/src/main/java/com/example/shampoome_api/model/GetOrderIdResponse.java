69f488bac2776220e95f6f740dd83f0b1b177948
Nihad Abou-Zid <nihad@prosaldo.net>
2022-06-04 12:07:26 +0200

    shampooMeAPI: corrected getOrderId response object

---
+++ shampooMe_API/src/main/java/com/example/shampoome_api/model/GetOrderIdResponse.java
@@ -0,0 +1,12 @@
+package com.example.shampoome_api.model;
+
+public class GetOrderIdResponse {
+    public String orderId;
+
+    public GetOrderIdResponse() {
+    }
+
+    public GetOrderIdResponse(String orderId) {
+        this.orderId = orderId;
+    }
+}
